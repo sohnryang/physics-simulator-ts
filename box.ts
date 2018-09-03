ae3803ac04697be9494105fbed08ddf430c440bc
손량 <loop.infinitely@gmail.com>
2018-09-03 10:15:33 +0000

    added a box

---
+++ box.ts
@@ -0,0 +1,15 @@
+/*
+ * box.ts -- a box
+ */
+
+import {Point} from './point';
+
+export class Box {
+    public minPoint: Point;
+    public maxPoint: Point;
+
+    constructor(minPoint: Point, maxPoint: Point) {
+        this.minPoint = minPoint;
+        this.maxPoint = maxPoint;
+    }
+}
