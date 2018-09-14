891e5c25b2b210bd1cdc618fb2906f453282f3b8
손량 <loop.infinitely@gmail.com>
2018-09-14 23:26:08 +0900

    added base class

--- src/circle.ts
+++ src/circle.ts
@@ -3,12 +3,14 @@
  */
 
 import { Point } from './point';
+import { Shape } from './shape';
 
-export class Circle {
+export class Circle extends Shape {
     public position: Point;
     public radius: number;
 
     constructor(position: Point, radius: number) {
+        super();
         this.position = position;
         this.radius = radius;
     }
